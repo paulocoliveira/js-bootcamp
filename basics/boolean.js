51ef56d50fcc9bd07eec6e479c178a5314a19d69
Paulo Oliveira <paulocol@gmail.com>
2018-09-06 00:40:58 -0300

    Boolean and number basics in javascript

---
+++ basics/boolean.js
@@ -0,0 +1,13 @@
+// === - equality operator
+// !== - no equal operator
+// <   - less than operator
+// >   - greater than operator
+// <=  - less than or equal to operator
+// >=  - greater than or equal to operator
+
+let age = 4
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
