94c84e2b81e7d4fecf3b2b169f6258b2dc05c2bc
Paulo Oliveira <paulocol@gmail.com>
2018-09-06 00:41:53 -0300

    If statement basics in javascript

---
+++ basics/if-statement.js
@@ -0,0 +1,11 @@
+let age = 66
+
+if (age <= 7) {
+    console.log('You will get a child discount!')
+    console.log(age)
+}
+
+if (age >= 65) {
+    console.log('You will get a senior discount!')
+    console.log(age)
+}
